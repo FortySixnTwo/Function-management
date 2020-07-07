ed9ca719c3d1ad0282f407c5a5517fad37c1f8fd
Forty Six and Two <07nhoyes@gmail.com>
2020-07-07 19:07:19 +0100

    asd

--- FunctionTool.py
+++ FunctionTool.py
@@ -8,18 +8,23 @@ Created on Tue Jul  7 12:14:24 2020
 """
 
 class Element(): # Class for elements of a dish & their allergen information
-    def __init__(self,
-                 Allium=bool,
-                 Gluten=bool,
-                 Shellfish=bool,
-                 Eggs=bool,
-                 Fish=bool,
-                 Lupin=False,
-                 Dairy=False,
-                 Molluscs=False,
-                 Mustard=False,
-                 Nuts=False,
-                 Peanuts=False,
-                 Sesame=False,
-                 Soya=False,
-                 Sulphites=False):
+    def __init__(self, Allium, Gluten, Shellfish, Eggs, Fish, Lupin, Dairy, Molluscs, Mustard, Nuts, Peanuts, Sesame, Soya, Sulphites):
+        self.Allium=False
+        self.Gluten=False
+        self.Shellfish=False
+        self.Eggs=False
+        self.Fish=False
+        self.Lupin=False
+        self.Dairy=False
+        self.Molluscs=False
+        self.Mustard=False
+        self.Nuts=False
+        self.Peanuts=False
+        self.Sesame=False
+        self.Soya=False
+        self.Sulphites=False
+
+
+CarrotPuree=Element(Dairy=True)
+
+print(CarrotPuree.Dairy)
